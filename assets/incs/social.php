7beda64adff332452c8eaca0b87c8a7484b08a39
Adriano Fialho <afialho.com@gmail.com>
2014-01-28 09:01:14 -0300

    launch v1

--- assets/incs/social.php
+++ assets/incs/social.php
@@ -1,3 +1,4 @@
+<a name="contato"></a>
 <div id="social">
   <div class="container">
     <div class="row centered">
@@ -9,7 +10,9 @@
           <a href="https://twitter.com/apimenti" data-toggle="tooltip" data-placement="bottom" title="Siga nosso Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
         </div>
         <div class="col-md-4">
-          <a href="contato" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco"><i class="fa fa-envelope"></i></a>
+          <!-- <a href="contato" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco"><i class="fa fa-envelope"></i></a> -->
+          Nosso email:
+          <a href="mailto:apimenti@apimenti.com.br" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco">apimenti@apimenti.com.br</a>
         </div>
       </div>
     </div>
--- index.php
+++ index.php
@@ -58,8 +58,8 @@
 		        <li class="active"><a href="" data-toggle="tooltip" data-placement="bottom" title="Apimenti - Coisas para internet">Apimenti</a></li>
 		        <li><a href="http://agilize.com.br" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Visitar o site da Agilize">Agilize</a></li>
 		        <li><a href="http://fazdelivery.com.br" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Visitar o site do fazDelivery">fazDelivery</a></li>
-		        <!-- <li><a href="s-engine" data-toggle="tooltip" data-placement="bottom" title="Conheça nosso motor de busca">S-ENGINE</a></li> -->
-		        <li><a href="contato" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco">Contato</a></li>
+		        <li><a href="#" data-toggle="modal" data-target="#modal-s-engine" data-placement="bottom" title="Conheça nosso motor de busca">S-ENGINE</a></li>
+		        <li><a href="#contato" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco">Contato</a></li>
 		      </ul>
 		    </div><!--/.nav-collapse -->
 		  </div>
@@ -94,7 +94,10 @@
 					<div class="col-lg-7 centered">
 						<h3>“Falar é fácil. Mostre-me o código” ― Linus Torvalds</h3>
 						<p>
-							
+							A Apimenti desenvolve coisas para internet.
+							<br />
+							Desde games, social apps, aplicações mobile ou web, passando por CMS's etc. Não importa muito o meio, tendo como destino a internet, sabemos
+							escrever boas linhas de código para transformar uma ideia em negócio.
 						</p>
 					</div>
 				</div>
@@ -139,10 +142,40 @@
 						</a>
 					</div>
 					<p>
-						Junto ao fazDelivery, desenvolvemos um motor de busca. Implantamos uma busca rápida, eficiente e inteligente em seu projeto.
+						Junto ao fazDelivery, desenvolvemos um motor de busca. O S-ENGINE combina diversos aspectos de uma busca.
 					</p>
 					<p>
-						<a href="contato" target="_blank" data-toggle="tooltip" data-placement="bottom" title="entre em contato">entre em contato</a>
+						<div id="modal-s-engine" class="modal fade">
+						  <div class="modal-dialog">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						        <h4 class="modal-title">S-ENGINE - motor de busca</h4>
+						      </div>
+						      <div class="modal-body" style="text-align:left;">
+						      	<p style="text-align:center;"><img src="assets/img/logo-s-engine.png" alt="S-ENGINE motor de busca"></p>
+						        <p>
+						        	O motor de busca S-ENGINE combina diversos aspectos de uma busca de delivery como dados
+									espaciais (distância entre o fornecedor e o cliente), dados textuais (descrição de produtos e
+									serviços), contexto (tráfego na hora do pedido, tempo de entrega estimando levando em
+									consideração a sobrecarga do fornecedor no momento do pedido), dados dos produtos (ex:
+									preço), relevância dos produtos (textual e escore dado pelos clientes) e uma análise semântica
+									da consulta (ex: pizza com queijo, que informa que o produto é pizza e que o ingrediente
+									principal é queijo). O grande desafio da busca é integrar todos esses componentes para sugerir
+									os melhores produtos para o usuário em um dado momento.
+						        </p>
+						        <p>
+						        	Para mais informações, envie-nos um email: 
+						        	<a href="mailto:apimenti@apimenti.com.br" data-toggle="tooltip" data-placement="bottom" title="Entre em contato conosco">apimenti@apimenti.com.br</a>
+						        </p>
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+						      </div>
+						    </div><!-- /.modal-content -->
+						  </div><!-- /.modal-dialog -->
+						</div><!-- /.modal -->
+						<a href="#" data-toggle="modal" data-target="#modal-s-engine" title="entre em contato">saiba mais</a>
 					</p>
 				</div>
 			</div>
@@ -160,18 +193,20 @@
 					<h3>APIMENTI... DE APIMENTAR</h3>
 					<div class="col-lg-10 col-lg-offset-1 mt">
 						<p>
-							4 sócios
-							(
 							<a href="http://afialho.com" target="_blank">Fialho</a>,
 							<a href="http://about.me/jailson" target="_blank">Jailson</a>,
-							<a href="https://www.facebook.com/rafael.a.silva.961556" target="_blank">Rafael</a>,
+							<a href="https://www.facebook.com/rafael.a.silva.961556" target="_blank">Rafael</a> e
 							<a href="http://colar.es/" target="_blank">Thiago</a>
-							)
 							fundaram em 2009 a Apimenti, com a proposta de levar mais tempeiro para internet.
-							Alguns frutos do nosso trabalho:
-							<a href="http://gruponotavel.com.br" target="_blank">Portal do Grupo Notável</a>,
-							<a href="http://www.ufba.br" target="_blank">Portal da UFBA</a>,
-							<a href="http://fazdelivery.com.br" target="_blank">fazDelivery</a> (projeto de inovação aprovado no edital da <a href="http://www.fapesb.ba.gov.br/" target="_blank">FAPESB</a>)
+						</p>
+						<p>
+							Algumas sementes plantadas:
+							<a href="http://agilize.com.br" target="_blank">Agilize</a> (Contabilidade pela internet),
+							<a href="http://fazdelivery.com.br" target="_blank">fazDelivery</a> (projeto de inovação aprovado no edital da <a href="http://www.fapesb.ba.gov.br/" target="_blank">FAPESB</a>),
+							<a href="http://touke.dreamhosters.com/" target="_blank">ToUke</a> (Tradutor de cifras para ukulelê),
+							<b>MudouAvisou</b> (serviço de notificação de encomendas dos Correios),
+							<a href="http://gruponotavel.com.br" target="_blank">Grupo Notável</a>,
+							<a href="http://www.ufba.br" target="_blank">UFBA</a>.
 						</p>
 					</div>
 				</div><!-- /row -->
